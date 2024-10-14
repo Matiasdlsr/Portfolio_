265fce60c9c0e4a6953bf7f03c808e0c2be3f0d6
matiasdlsr <166665626+Matiasdlsr@users.noreply.github.com>
2024-10-14 03:51:24 -0300

    :add: project miGuardia

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -5,7 +5,7 @@ import Particle from "../Particle";
 import leaf from "../../Assets/Projects/leaf.png";
 import emotion from "../../Assets/Projects/emotion.png";
 import editor from "../../Assets/Projects/codeEditor.png";
-import chatify from "../../Assets/Projects/chatify.png";
+import miGuardia from "../../Assets/Projects/mi_Guardia.png";
 import suicide from "../../Assets/Projects/suicide.png";
 import bitsOfCode from "../../Assets/Projects/blog.png";
 
@@ -23,11 +23,11 @@ function Projects() {
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={chatify}
+              imgPath={miGuardia}
               isBlog={false}
-              title="Chatify"
-              description="Personal Chat Room or Workspace to share resources and hangout with friends build with react.js, Material-UI, and Firebase. Have features which allows user for realtime messaging, image sharing as well as supports reactions on messages."
-              ghLink="https://github.com/soumyajit4419/Chatify"
+              title="miGuardia"
+              description="Brinda información en tiempo real sobre el estado de las guardias médicas más cercanas, facilitando la búsqueda de atención según la necesidad del usuario, mejorando la accesibilidad a la información médica y contribuyendo a la eficiencia en la atención medica.."
+              ghLink="https://github.com/Matiasdlsr/FundacionPescar-miGuardia"
               demoLink="https://chatify-49.web.app/"
             />
           </Col>
