9b7cbcda1081e01ec191a75e1e86ba252236d724
matiasdlsr <matiasdlsr@gmail.com>
2025-04-22 18:44:31 -0300

    :change: cv

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -3,7 +3,7 @@ import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
 
-import pdf from "../../Assets/Projects/delSeñorMatias_Cv.pdf";
+import pdf from "../../Assets/Projects/Cv_delSeñor.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
